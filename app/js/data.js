271cdf5b50b36c172bd080bc2f30449afda0dc15
Kawsar Ahamed <kawsar019444@gmail.com>
2023-07-07 21:34:49 +0600

    all file done

---
+++ app/js/data.js
@@ -0,0 +1,50 @@
+const questions = [
+    {
+        id: 1,
+        question: "What is the best IT center in Bangladesh?",
+        answer:[
+            { text: "CodemanBD", correct: "false" },
+            { text: "SoroBindu", correct: "true" },
+            { text: "Ostad", correct: "false" },
+            { text: "Webcoder It", correct: "false"}
+
+        ]
+    },
+    
+    {
+        id : 2,
+        question: "Who is the best teacher of SoroBindu?",
+        answer:[
+            { text: "Ashraful Haque", correct: "true" },
+            { text: "Jillur Rahman", correct: "false" },
+            { text: "Emon Khan", correct: "false" },
+            { text: "Ariful Islam", correct: "false"}
+
+        ]
+    },
+
+    {
+        id : 3,
+        question: "Who is the founder of SoroBindu?",
+        answer:[
+            { text: "Emon Khan", correct: "false" },
+            { text: "Ashraful Haque", correct: "false" },
+            { text: "Jillur Rahman", correct: "true" },
+            { text: "Ariful Islam", correct: "false"}
+
+        ]
+    },
+
+    {
+        id : 4,
+        question: "What is famous for Pabna district?",
+        answer:[
+            { text: "President Mohammed Shahabuddin", correct: "false" },
+            { text: "Rooppur Nuclear Power Plant", correct: "false" },
+            { text: "SQUARE Group", correct: "false" },
+            { text: "Mental Hospital", correct: "true"}
+
+        ]
+    },
+
+];
---
+++ app/js/main.js
@@ -0,0 +1,83 @@
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-button");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML="Next";
+    showQuestion();
+};
+
+function showQuestion() {
+
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answer.forEach((answer) => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButton.appendChild(button);
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    })
+};
+
+function resetState() {
+    nextButton.style.display = "none";
+    while (answerButton.firstChild) {
+        answerButton.removeChild(answerButton.firstChild);
+    }
+}
+function selectAnswer(e) {
+    const selectedButton = e.target;
+    const iscorrect = selectedButton.dataset.correct==="true";
+    if (iscorrect) {
+        selectedButton.classList.add("correct");
+        score++;
+    } else {
+        selectedButton.classList.add("incorrect");
+    }
+    Array.from(answerButton.children).forEach((button) => {
+        if (button.dataset.correct === "true") {
+            button.classList.add("correct");
+        }
+        button.disabled = "true";
+    })
+    nextButton.style.display = "block";
+}
+
+function showScore() {
+    resetState();
+    questionElement.innerHTML = `You are scored ${score} out of ${questions.length} !`;
+    nextButton.innerHTML= "Play Again"
+    nextButton.style.display = "block";
+};
+function handleNextButton() {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+        showQuestion();
+    } else {
+       showScore();
+    }
+};
+
+
+
+nextButton.addEventListener("click", ()=>{
+   
+    if (currentQuestionIndex < questions.length) {
+       handleNextButton();
+    } else {
+        startQuiz();
+    }
+});
+showQuestion();
